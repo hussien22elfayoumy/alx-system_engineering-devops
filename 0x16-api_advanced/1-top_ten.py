14f5ad22b922c4566c2893d4645aad685baa78ed
hussien22elfayoumy <hussienmedo36@gmail.com>
2024-08-13 21:45:44 +0300

    nice

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,12 +5,20 @@ import requests
 def top_ten(subreddit):
     """function that print the titles and top10"""
 
-    sub_info = requests.get("https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-                            .format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
-    if sub_info.status_code >= 300:
-        print('None')
+    link = "https://www.reddit.com/r/{subreddit}/top.json".format(subreddit)
+    
+    heads = {'User-Agent': 'Mozilla/5.0'}
+
+    data = requests.get(link, headers=heads, params={"limit": 10})
+
+    if data.status_code != 200:
+        print(None)
+        return
+
+    usr_posts = data.json().get("data").get("children")
+
+    if usr_posts:
+        for post in usr_posts:
+            print(post.get("data").get("title"))
     else:
-        [print(child.get("data").get("title"))
-         for child in sub_info.json().get("data").get("children")]
+        print(None)
