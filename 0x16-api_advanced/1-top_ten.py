490ea1ce40b38a1f6a1e0a55d97d1dc4ea3c4ec1
hussien22elfayoumy <hussienmedo36@gmail.com>
2024-08-13 21:41:54 +0300

    yolo

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,16 @@
+"""Task 1 - module to return top ten hot posts"""
+
+
+def top_ten(subreddit):
+    """function that print the titles and top10"""
+    import requests
+
+    sub_info = requests.get("https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
+        print('None')
+    else:
+        [print(child.get("data").get("title"))
+         for child in sub_info.json().get("data").get("children")]
