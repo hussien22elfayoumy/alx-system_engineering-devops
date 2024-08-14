933a1eb49336d99e34a64733d6eb288280f73904
hussien22elfayoumy <hussienmedo36@gmail.com>
2024-08-15 00:14:55 +0300

    nice

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
-# Fixing the error bad `phpp` extensions to `php` in the `wp-settings.php`.
+# Fixing the error bad `phpp` extensions in /var/www/html/wp-settings.php'
 exec {'replace':
   provider => shell,
   command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
