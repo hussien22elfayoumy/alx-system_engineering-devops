571fcfcd157e2e639833d9d659993e7085c3ff02
hussien22elfayoumy <hussienmedo36@gmail.com>
2024-08-14 14:34:13 +0300

    yalla

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Fixes bad `phpp` extensions replacing "phpp" to "php"
 
 exec { 'fixes-problem':
-  command => 'sed -i 's/phpp/php/g' /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
+  command => "sed -i s/phpp/php/g /var/www/html/wp-settings.php",
+  path    => "/usr/local/bin/:/bin/"
 }
