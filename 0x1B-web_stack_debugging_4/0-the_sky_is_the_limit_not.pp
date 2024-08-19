f39487e1b50f07db0441bc6e29f7e9cb304303a2
hussien22elfayoumy <hussienmedo36@gmail.com>
2024-08-19 11:18:57 +0300

    noce

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+# fixes the nginx problem to accept and serve more and more requests
+
+exec {'modify max open files limit setting':
+  command => 'sed -i "s/15/10000/" /etc/default/nginx && sudo service nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
+}
